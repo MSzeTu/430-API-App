e854db7e8a77bf0bc08dc14bf915a7d044c69831
Matthew Sze-Tu <ms1937@rit.edu>
2021-09-28 22:07:51 -0400

    Adding events now works, but I forgot to make you able to actually get them afterwards

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -7,7 +7,7 @@ const respondJSON = (request, response, status, object) => {
   response.end();
 };
 
-// Returns without JSON Body, takes request response and status. Probably won't need this
+// Returns without JSON Body, takes request response and status.
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
@@ -18,15 +18,37 @@ const getEvent = (request, response) => {
   const responseJSON = {
     events,
   };
+  
 
   respondJSON(request, response, 200, responseJSON);
 };
 
-//Adds an event with a date, binds the guest list to it
+// Adds an event with a date, binds the guest list to it. Change to allow for Head Requests
 const addEvent = (request, response, body) => {
+  const responseJSON = {
+    message: 'You must have an event name and date.'
+  };
+  console.dir(body);
+  if(!body.name || !body.date) {
+    responseJSON.id = 'missingParams';
+    console.log("missingParams");
+    return respondJSON(request, response, 400, responseJSON);
+  }
 
+  let responseCode = 201;
 
-}
+  //For now, only create objects
+  events[body.name] = {
+    "name" : body.name,
+    "date" : body.date,
+    "guests" : [] 
+  }
+  for(let i=0; i <= body.guestNum; i++)
+  {
+    events[body.name].guests.push(body.guestList[i]);
+  }
+  return respondJSON(request, response, responseCode, responseJSON);
+};
 
 // For nonexistent Pages
 const notReal = (request, response) => {
